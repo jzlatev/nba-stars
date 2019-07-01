2e6e52bd3dfc10ab9dcefb06d5ab3b97350e7538
jzlatev <52425187+jzlatev@users.noreply.github.com>
2019-07-01 21:58:08 +0300

    Init

---
+++ NBA Stars/Player.cs
@@ -0,0 +1,69 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NBA_Stars
+{
+    class Player: IEquatable<Player>, IComparable<Player>
+    {
+        private string name;
+        private int since;
+        private string position;
+        private int rating;
+    
+        public void loadFromArray(JObject obj)
+        {
+            name = (string)obj.Property("Name").Value;
+            since = (int)obj.Property("PlayingSince").Value;
+            position = (string)obj.Property("Position").Value;
+            rating = (int)obj.Property("Rating").Value;
+        }
+
+        public bool hasMoreRatingThan(int rating)
+        {
+            if (this.rating >= rating)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool hasMoreExperienceThan(int years,int currentYear)
+        {
+            if (years<=currentYear-this.since)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public string getName()
+        {
+            return name;
+        }
+        public int getRating()
+        {
+            return rating;
+        }
+
+        int IComparable<Player>.CompareTo(Player other)
+        {
+            if (other == null)
+            {
+                return 1;
+            }
+            else
+            {
+                return this.rating.CompareTo(other.getRating());
+            }
+        }
+
+        public bool Equals(Player other)
+        {
+            return other.getRating() == this.rating;
+        }
+    }
+}
---
+++ NBA Stars/Program.cs
@@ -0,0 +1,62 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace NBA_Stars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int currentYear = (int)DateTime.Now.Year;
+
+            string path = args[0];
+            string outputPath;
+            int rating;
+            int requiredExperience;
+            string input;
+            if (!File.Exists(path))
+            {
+                throw new Exception("Missing file");
+            }
+
+            Console.Write("The maximum number of years the player has played in the league to qualify: ");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out requiredExperience);
+
+            Console.Write("The minimum rating the player should have to qualify: ");
+            input = Console.ReadLine();
+            Int32.TryParse(input, out rating);
+
+            Console.Write("Path to the generated CSV file: ");
+
+            outputPath = Console.ReadLine();
+            
+            string jsonString = System.IO.File.ReadAllText(path);
+
+            List<Player> list = new List<Player>();
+            JArray jsonArray = JArray.Parse(jsonString);
+            foreach (JObject obj in jsonArray.Children<JObject>())
+            {                Player player = new Player();
+                player.loadFromArray(obj);
+                if (player.hasMoreRatingThan(rating) && player.hasMoreExperienceThan(requiredExperience,currentYear))
+                {
+                    list.Add(player);
+                }
+                                 
+            }
+
+            list.Sort();
+            list.Reverse();
+
+            var csv = new StringBuilder();
+            csv.AppendLine(string.Format("Name,Rating"));
+            list.ForEach((player)=>csv.AppendLine(string.Format("{0}, {1}",player.getName(),player.getRating())));
+
+            File.WriteAllText(outputPath, csv.ToString());
+        }
+    }
+}
